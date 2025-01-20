50d5694d97908a9922385f0bc60f9151ca491b12
matheus mantovani <mantovanimatheus335@gmail.com>
2025-01-20 16:08:39 -0300

    activities do commit anterior

---
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisadeopiniao/ui/checar/ChecarActivity.kt
@@ -0,0 +1,13 @@
+package br.edu.ifsp.dmo.pesquisadeopiniao.ui.checar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.pesquisadeopiniao.R
+
+class ChecarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisadeopiniao/ui/participar/ParticiparActivity.kt
@@ -0,0 +1,13 @@
+package br.edu.ifsp.dmo.pesquisadeopiniao.ui.participar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.pesquisadeopiniao.R
+
+class ParticiparActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
