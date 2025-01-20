f26b1d4dc5c4bae97fdb8a307db919f3b6a8e1fe
matheus mantovani <mantovanimatheus335@gmail.com>
2025-01-20 16:08:06 -0300

    layout das activities (main, participar, votar, checar, finalizar)

---
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisadeopiniao/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.edu.ifsp.dmo.pesquisadeopiniao.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo.pesquisadeopiniao.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
