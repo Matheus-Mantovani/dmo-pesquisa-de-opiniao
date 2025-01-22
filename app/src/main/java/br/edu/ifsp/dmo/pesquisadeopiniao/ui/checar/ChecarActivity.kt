6e7578e1e520b78b0177dda242b758047982d260
matheus mantovani <mantovanimatheus335@gmail.com>
2025-01-22 17:40:49 -0300

    botao de voltar à tela inicial em ChecarActivity

--- app/src/main/java/br/edu/ifsp/dmo/pesquisadeopiniao/ui/checar/ChecarActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo/pesquisadeopiniao/ui/checar/ChecarActivity.kt
@@ -1,12 +1,16 @@
 package br.edu.ifsp.dmo.pesquisadeopiniao.ui.checar
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import br.edu.ifsp.dmo.pesquisadeopiniao.R
 import br.edu.ifsp.dmo.pesquisadeopiniao.databinding.ActivityChecarBinding
+import br.edu.ifsp.dmo.pesquisadeopiniao.ui.main.MainActivity
+import br.edu.ifsp.dmo.pesquisadeopiniao.utils.Constants
 
 class ChecarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChecarBinding
@@ -23,30 +27,39 @@ class ChecarActivity : AppCompatActivity() {
     }
 
     private fun configOnClickListener() {
-        binding.buttonChecar.setOnClickListener {
-            val id = binding.edixtextCodigo.text.toString()
-            if(idValido(id)) {
-                val voto = viewModel.buscarId(id)
-                AlertDialog.Builder(this)
-                    .setTitle(getString(R.string.sua_opiniao))
-                    .setMessage(voto!!.opiniao.valor())
-                    .setNeutralButton(getString(R.string.fechar)) { dialog, _ ->
-                        dialog.dismiss()
-                    }
-                    .show()
-            } else {
-                Toast.makeText(
-                    this,
-                    getString(R.string.id_nao_encontrado),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
+        binding.buttonChecar.setOnClickListener { checarId() }
+
+        binding.buttonVoltar.setOnClickListener { voltarParaTelaInicial() }
     }
 
     private fun idValido(id: String): Boolean {
         return viewModel.buscarId(id) != null
     }
 
+    private fun voltarParaTelaInicial() {
+        val mIntent = Intent(this, MainActivity::class.java)
+        Log.i(Constants.KEY_TESTE, "voltar pra tela inicial ")
+        startActivity(mIntent)
+        finish()
+    }
 
+    private fun checarId() {
+        val id = binding.edixtextCodigo.text.toString()
+        if(idValido(id)) {
+            val voto = viewModel.buscarId(id)
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.sua_opiniao))
+                .setMessage(voto!!.opiniao.valor())
+                .setNeutralButton(getString(R.string.fechar)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+        } else {
+            Toast.makeText(
+                this,
+                getString(R.string.id_nao_encontrado),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }
